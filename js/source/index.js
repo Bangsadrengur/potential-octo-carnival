e2c0a639ec0938c8aead62ddf7a1d3e34a3c408b
Bangsadrengur <bangsadrengur@BW1768-T550.lan>
2018-04-21 00:58:51 +0000

    compiles elm pages before starting; improves error handling

---
+++ js/source/index.js
@@ -0,0 +1,27 @@
+const MangaEden = require('./manga-eden');
+const Mangafreak = require('./mangafreak');
+const R = require('ramda');
+
+const source = (favorites) => {
+  const sources = {
+    'manga-eden': MangaEden(R.pluck('lookup', favorites)),
+    mangafreak: Mangafreak(R.pluck('lookup', favorites)),
+  };
+
+  const getPage = (serie, chapter, page) =>
+    sources[favorites[serie].source].getPage(serie, chapter, page);
+
+  const getPages = (serie, chapter) =>
+    sources[favorites[serie].source].getPages(serie, chapter);
+
+  const getChapters = serie =>
+    sources[favorites[serie].source].getChapters(serie);
+
+  return {
+    getPage,
+    getPages,
+    getChapters,
+  };
+};
+
+module.exports = source;
--- test/source.js
+++ test/source.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const { series, chapters, pages } = require('./fixtures/example-data');
+const { SERIES, chapters } = require('./fixtures/example-data');
 const {
   parseSeries,
   parseChaptersUrl,
@@ -7,28 +7,28 @@ const {
   chapterToChapterPath,
   selectChapter,
   pagesToPagePath,
-} = require('../lib/source/parsers');
+} = require('../js/source/manga-eden/parsers');
 
-test('parseSeries', t => {
-  const output = parseSeries(JSON.stringify(series));
+test('parseSeries', (t) => {
+  const output = parseSeries(JSON.stringify(SERIES));
 
   t.deepEqual(output, [
-    { title: "Flower Dream", id: "5372389645b9ef5a0b1d20d8" },
-    { title: "Kanai-kun", id: "54430be945b9ef3a6d5818cc" }
-  ])
+    { title: 'Flower Dream', id: '5372389645b9ef5a0b1d20d8' },
+    { title: 'Kanai-kun', id: '54430be945b9ef3a6d5818cc' },
+  ]);
 });
 
-test('parseChaptersUrl', t => {
+test('parseChaptersUrl', (t) => {
   const series = [
-    { title: "Flower Dream", id: "5372389645b9ef5a0b1d20d8" },
-    { title: "Kanai-kun", id: "54430be945b9ef3a6d5818cc" }
+    { title: 'Flower Dream', id: '5372389645b9ef5a0b1d20d8' },
+    { title: 'Kanai-kun', id: '54430be945b9ef3a6d5818cc' },
   ];
   const output = parseChaptersUrl('Flower Dream')(series);
 
   t.is(output, '5372389645b9ef5a0b1d20d8');
 });
 
-test('parseChapters', t => {
+test('parseChapters', (t) => {
   const output = parseChapters(JSON.stringify(chapters));
 
   t.deepEqual(output, [
@@ -37,10 +37,10 @@ test('parseChapters', t => {
     { number: 3, name: '3', path: '4e711cacc09225616d037c72' },
     { number: 2, name: '2', path: '4e711cb3c09225616d037d05' },
     { number: 1, name: '1', path: '4e711caec09225616d037ca0' },
-  ])
+  ]);
 });
 
-test('selectChapter', t => {
+test('selectChapter', (t) => {
   const parsedChapters = [
     { number: 5, name: '5', path: '4e711cb0c09225616d037cc2' },
     { number: 4, name: '4', path: '4e711cb1c09225616d037ce4' },
@@ -52,11 +52,11 @@ test('selectChapter', t => {
 
   t.deepEqual(
     output,
-    [{ number: 2, name: '2', path: '4e711cb3c09225616d037d05' }]
+    [{ number: 2, name: '2', path: '4e711cb3c09225616d037d05' }],
   );
 });
 
-test('chapterToChapterPath', t => {
+test('chapterToChapterPath', (t) => {
   const parsedChapter = [
     { number: 4, name: '4', path: '4e711cb1c09225616d037ce4' },
   ];
@@ -65,7 +65,7 @@ test('chapterToChapterPath', t => {
   t.deepEqual(output, '4e711cb1c09225616d037ce4');
 });
 
-test('pagesToPagePath', t => {
+test('pagesToPagePath', (t) => {
   const pages = [
     { number: 1, path: 'path-1.jpg', height: 1800, width: 1108 },
     { number: 2, path: 'path-2.jpg', height: 800, width: 1000 },
